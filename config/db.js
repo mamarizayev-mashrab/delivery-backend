ec6f33904bb9621975f6f038772435adfdfce6f5
Mashrab Mamarizayev <mashrabmamarizayev@gmail.com>
2025-06-18 22:52:12 +0500

    fix: change mongodb url

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://mashrabmamarizayev:4R2A6UEPjjdydkTo@cluster0.v9sgbu2.mongodb.net/food-del').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect('mongodb+srv://mashrabmamarizayev:8xviy3YgmFr2z449@cluster0.5lcgu09.mongodb.net/authApp?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DataBase Connected"));
 }
